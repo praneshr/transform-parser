97017e7f1b589309bb80a889f6fc7e44e4805121
Pranesh Ravi <praneshpranesh@gmail.com>
2019-08-28 14:05:11 +0530

    Adding code docs

--- index.ts
+++ index.ts
@@ -1,13 +1,30 @@
-const ignoredKeys = ['scale', 'scale3d', 'scaleX', 'scaleY', 'scaleZ'];
+export interface TransformObject {
+  [key: string]: number | string | (string | number)[] | boolean;
+}
 
-const parse = (transformString: string): Record<string, any> => {
-  const transforms = transformString.split(/\) |\)/);
+// List of transforms for which the value has to be numeral. Default px substitution for these
+// transforms will be skipped.
+const transformsWithNumeralValues = ['scale', 'scale3d', 'scaleX', 'scaleY', 'scaleZ'];
+
+/**
+ * Returns a object representation of the transform string supplied. It parses the string and
+ * converts them to an object.
+ * @param transformString String containing the transforms
+ */
+const parse = (transformString: string): TransformObject => {
+  const transforms = transformString.trim().split(/\) |\)/);
+  // Handle "initial", "inherit", "unset".
   if (transforms.length === 1) {
     return {
       [transforms[0]]: true,
     };
   }
 
+  /**
+   * Converts string values to number when the unit is pixel or parsable. Returns a string when the
+   * unit is not pixel or not parsable.
+   * @param value Value of a transform
+   */
   const parsePixelValues = (value: string): string | number => {
     if (value.endsWith('px')) return parseFloat(value);
     if (!Number.isNaN(Number(value))) return Number(value);
@@ -28,9 +45,13 @@ const parse = (transformString: string): Record<string, any> => {
   }, {});
 };
 
-const stringify = (transformObject: Record<string, number | string | boolean | (string | number)[]>): string => {
+/**
+ * Returns the transform string constructed from the transform object supplied.
+ * @param transformObject Object containing the transforms
+ */
+const stringify = (transformObject: TransformObject): string => {
   const resolveUnits = (key: string, val: number | string): string => {
-    if (typeof val === 'number' && !ignoredKeys.includes(key)) {
+    if (typeof val === 'number' && !transformsWithNumeralValues.includes(key)) {
       return `${val}px`;
     }
     return val.toString();
@@ -41,10 +62,11 @@ const stringify = (transformObject: Record<string, number | string | boolean | (
     if (value === true) return key;
     if (value === false) return acc;
     if (Array.isArray(value)) {
-      return `${acc} ${key}(${value.map((val) => resolveUnits(key, val)).join(', ')})`.trim();
+      return `${acc} ${key}(${value.map((val) => resolveUnits(key, val)).join(', ')})`;
     }
-    return `${acc} ${key}(${resolveUnits(key, value)})`.trim();
-  }, '');
+    return `${acc} ${key}(${resolveUnits(key, value)})`;
+  }, '')
+    .trim();
 };
 
 export { parse, stringify };
